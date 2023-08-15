ea50ed025c3174b0f660f1bde8c889d2d85f345d
Kalène Apiou <apiou.kalene@gmail.com>
2023-08-15 14:56:52 +0200

    add redirection animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useRef } from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
@@ -15,26 +16,58 @@ import Nav from "./components/Nav";
 import "./App.scss";
 
 function App() {
+    const redirect = useRef();
+    const urlDeRedirection = "https://www.kalene-apiou.com/";
+    if (window.location.href !== urlDeRedirection) {
+        setTimeout(() => {
+          window.location.href = urlDeRedirection;
+          redirect.current.style.display = "none"
+        redirect.current.style.opacity = "0"
+        document.body.style.overflow = "hidden"
+        }, 1500);
+      }
+
     return (
-        <div className="stack background">
-            <Nav />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/work" element={<Work />} />
-                <Route path="/contact" element={<Contact />} />
-                <Route
-                    path="/work/discovermostsuitableproperty"
-                    element={<DiscoverMostSuitableProperty />}
-                />
-                <Route path="/work/gymwebsite" element={<GymWebsite />} />
-                <Route path="/work/socialnetwork" element={<SocialNetwork />} />
-                <Route path="/work/immobilier" element={<Immobilier />} />
-                <Route path="/work/animationtext" element={<AnimationText />} />
-                <Route path="/work/portfolio" element={<Portfolio />} />
-                <Route path="/work/kanap" element={<Kanap />} />
-            </Routes>
-        </div>
+        <>
+            <div ref={redirect} className="redirection">
+                <p class="circle">
+                    <span class="ouro ouro3">
+                        <span class="left">
+                            <span class="anim"></span>
+                        </span>
+                        <span class="right">
+                            <span class="anim"></span>
+                        </span>
+                    </span>
+                </p>
+                <h1>Redirection...</h1>
+            </div>
+            <div className="stack background">
+                <Nav />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/work" element={<Work />} />
+                    <Route path="/contact" element={<Contact />} />
+                    <Route
+                        path="/work/discovermostsuitableproperty"
+                        element={<DiscoverMostSuitableProperty />}
+                    />
+                    <Route path="/work/gymwebsite" element={<GymWebsite />} />
+                    <Route
+                        path="/work/socialnetwork"
+                        element={<SocialNetwork />}
+                    />
+                    <Route path="/work/immobilier" element={<Immobilier />} />
+                    <Route
+                        path="/work/animationtext"
+                        element={<AnimationText />}
+                    />
+                    <Route path="/work/portfolio" element={<Portfolio />} />
+                    <Route path="/work/kanap" element={<Kanap />} />
+                </Routes>
+            </div>
+        </>
     );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 import "./styles/index.scss";
 import "./styles/global.scss";
 
+
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
         <BrowserRouter>
